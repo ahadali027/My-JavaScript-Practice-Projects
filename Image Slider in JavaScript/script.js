b4e32ca49c5677a846196c7f4bfff4830bd5014e
Ahad Ali <ahadali.p027@gmail.com>
2024-02-17 20:48:46 +0500

    new update

--- Image Slider in JavaScript/script.js
+++ Image Slider in JavaScript/script.js
@@ -1,4 +1,6 @@
-flag = 0;
+let flag = 0; // 1
+
+// 1
 
 function controller(x) {
   flag = flag + x;
@@ -6,19 +8,23 @@ function controller(x) {
 }
 
 function slideshow(num) {
+  // 1
   let slid = document.getElementsByClassName("slid");
+
+  console.log(num);
 
   if (num == slid.length) {
     flag = 0;
     num = 0;
   }
+
   if (num < 0) {
     flag = slid.length - 1;
     num = slid.length - 1;
   }
 
-  for (let y of slid) {
-    y.style.display = "none";
+  for (let singleSlide of slid) {
+    singleSlide.style.display = "none";
   }
   slid[num].style.display = "block";
 }
